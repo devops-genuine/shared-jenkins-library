12f9d393be187ee66c5a0d5fd852dea241035252
jesada.gonkratoke <selfieblue@gmail.com>
2020-05-03 17:35:57 +0700

    fix

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -9,18 +9,5 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
  * @param targetNamespace a terget kubernetes namespace
  */
 def call(RunWrapper currentBuild, String repoName, String projectName, String releaseVersion, String targetNamespace) {
-
-    def currentResult = currentBuild.currentResult
-    def previousResult = currentBuild.getPreviousBuild()?.getResult()
-
-    def buildIsFixed =
-        currentResult == Result.SUCCESS.toString() &&
-        currentResult != previousResult &&
-        previousResult != null
-
-    def badResult =
-        currentResult in [Result.UNSTABLE.toString(), Result.FAILURE.toString()]
-    if (buildIsFixed || badResult) {
-    	sh "/usr/local/bin/helm upgrade --install ${projectName} ./${projectName} --set image.repository=${repoName}/${projectName} --set image.tag=${releaseVersion}-${env.BUILD_NUMBER} -n ${targetNamespace}"
-    }
+    sh "/usr/local/bin/helm upgrade --install ${projectName} ./${projectName} --set image.repository=${repoName}/${projectName} --set image.tag=${releaseVersion}-${env.BUILD_NUMBER} -n ${targetNamespace}"
 }
