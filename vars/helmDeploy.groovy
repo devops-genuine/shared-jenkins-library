78d7f1a42df8b84981819626137cb139b3a665b7
jesada.gonkratoke <selfieblue@gmail.com>
2020-05-03 17:24:03 +0700

    Add helm

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -6,8 +6,9 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
  * @param repoName an docker repo name
  * @param projectName an application name
  * @param releaseVersion an release version's number
+ * @param targetNamespace a terget kubernetes namespace
  */
-def call(RunWrapper currentBuild, String repoName, String projectName, String releaseVersion) {
+def call(RunWrapper currentBuild, String repoName, String projectName, String releaseVersion, String targetNamespace) {
 
     def currentResult = currentBuild.currentResult
     def previousResult = currentBuild.getPreviousBuild()?.getResult()
@@ -20,9 +21,6 @@ def call(RunWrapper currentBuild, String repoName, String projectName, String re
     def badResult =
         currentResult in [Result.UNSTABLE.toString(), Result.FAILURE.toString()]
     if (buildIsFixed || badResult) {
-    	sh "/usr/local/bin/aws ecr create-repository --repository-name ${projectName} --image-tag-mutability IMMUTABLE || true"
-    	sh "/usr/local/bin/aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${repoName}/${projectName}"
-    	sh "docker image build -t ${repoName}/${projectName}:${releaseVersion}-${env.BUILD_NUMBER} ."
-	    sh "docker image push ${repoName}/${projectName}:${releaseVersion}-${env.BUILD_NUMBER}"
+    	sh "helm upgrade --install ${projectName} ./${projectName} --set image.repository=${repoName}/${projectName} --set image.tag=${releaseVersion}-${env.BUILD_NUMBER} -n ${targetNamespace}"
     }
 }
